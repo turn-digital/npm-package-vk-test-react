68aa701e6326899f49a13031f9af891ce602bcff
DoofisS <thevlad2698@gmail.com>
2024-04-23 22:52:27 +0300

    adding all files

--- src/App.js
+++ src/App.js
@@ -1,23 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+
+import "@doofiss/web-components-test-vk/es/components/button/index.js";
+import "@doofiss/web-components-test-vk/es/components/tooltip/index.js";
+import "@doofiss/web-components-test-vk/es/components/turn-status-indicator/index.js";
+import "@doofiss/web-components-test-vk/es/components/turn-section-title/index.js";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <cds-section-title
+        title="Propsed Title"
+        tooltipText="Propsed Tooltip Text"
+        showTooltip={true}
+      ></cds-section-title>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <cds-button>Button</cds-button>
+      <br />
+      <br />
+      <br />
+      <br />
+      <br />
+      <cds-status-indicator
+        statusIcon="checkmark--filled-green"
+        statusText="Gaida parakstu no klienta"
+        date="01.01.2024"
+      ></cds-status-indicator>
+      <cds-status-indicator
+        statusIcon="checkmark--filled-blue"
+        statusText="Gaida parakstu no klienta"
+      ></cds-status-indicator>
+      <br />
+      <br />
+      <br />
     </div>
   );
 }
